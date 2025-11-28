0a13eeb4e15a9ae39a710fe9f4915c7d0e832c00
Jean Henrique <76601434+JeanHenriqueSz@users.noreply.github.com>
2025-11-28 20:02:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -37,13 +37,13 @@ st.markdown("""
 ">
 <b>Equipe de Desenvolvimento</b><br><br>
 
-Laura Fagundes Freitas — 42413265 <br>
 Aparício Virginio do Amaral — 42414535 <br>
-Matheus Henrique Santos — 42410613 <br>
-Danielly Silva Teixeira — 42415112 <br>
-Samuel Fellipe Batista — 42521948 <br>
 Arthur Maestro da Silva Aguiar — 4231925821 <br>
+Danielly Silva Teixeira — 42415112 <br>
 Jean Henrique Resende Paiva — 4251925073 <br>
+Laura Fagundes Freitas — 42413265 <br>
+Matheus Henrique Santos — 42410613 <br>
+Samuel Fellipe Batista — 42521948 <br>
 
 </div>
 """, unsafe_allow_html=True)
